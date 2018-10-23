ed475e3baca6242242ff92960db67933b65afb38
jstricklin89 <jstricklin89@gmail.com>
2018-10-23 14:48:03 -0500

    bootstrap changes and folder changes

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,22 @@
-document.addEventListener('DOMContentLoaded', () => {
-  const endPoint = 'http://localhost:3000/api/v1/users'
-  fetch(endPoint)
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(user => {
-      const newUser = new User(user)
-      document.getElementById('user-list').innerHTML += newUser.showUser()
-    })
-  })
-})
-
+document.addEventListener("DOMContentLoaded", () => {
+  const userURL = "http://localhost:3000/api/v1/users";
+  fetch(userURL)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(user => {
+        const newUser = new User(user);
+        document.getElementById("user-list").innerHTML += newUser.showUser();
+      });
+    });
+  const questionURL = "http://localhost:3000/api/v1/questions";
+  fetch(questionURL)
+    .then(response => response.json())
+    .then(data => {
+      data.forEach(question => {
+        const newQuestion = new Question(question);
+        document.getElementById(
+          "question"
+        ).innerHTML += newQuestion.showQuestion();
+      });
+    });
+});
