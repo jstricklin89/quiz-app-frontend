2a1aeacfdb0e16a8181ee2e98b33fc61902767f2
kevinptx <kevin.peery@gmail.com>
2018-10-24 11:05:58 -0500

    committing so I can pull

--- app/js/category.js
+++ app/js/category.js
@@ -1,10 +1,13 @@
 class Category {
   constructor(category) {
     this.name = category.name;
+    this.img = category.img;
     Category.all.push(this);
   }
 
   showCategory() {
+    let cardBody = document.getElementById("card-body");
+    cardBody.innerText = category.name;
     return `<p>This is the Category: ${this.name}</p>`;
   }
 }
