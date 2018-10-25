31f854b564895ae5d9c8dc3dcde41b278ef79f42
kevinptx <kevin.peery@gmail.com>
2018-10-25 12:47:00 -0500

    notes on procedures added

--- app/js/index.js
+++ app/js/index.js
@@ -1,12 +1,14 @@
 let questions = [];
 let filteredQuestions = [];
+let users = [];
 document.addEventListener("DOMContentLoaded", () => {
   const userURL = "http://localhost:3000/api/v1/users";
   fetch(userURL)
     .then(response => response.json())
     .then(data => {
       data.forEach(user => {
-        const newUser = new User(user);
+        users.push(new User(user));
+
         // document.getElementById("user-list").innerHTML += newUser.showUser();
       });
     });
@@ -39,9 +41,45 @@ document.addEventListener("DOMContentLoaded", () => {
         // ).innerHTML += newAnsweredQuestion.showAnsweredQuestion();
       });
     });
-    document.getElementById('question-form').style.display = "none"
-    document.getElementById('category-id').style.display = "none"
+  document.getElementById("question-form").style.display = "none";
+  document.getElementById("category-id").style.display = "none";
 });
+
+function showLogin() {
+  const signUp = document.getElementById("sign-up");
+  const login = document.getElementById("login");
+  const logout = document.getElementById("logout");
+  const navbarNav = document.getElementById("navbarNav");
+  // navbarNav.addEventListener("click", function(event) {
+  //   console.log("CLICKED");
+  // });
+
+  if (event.target.id === "sign-up") {
+    ///the top part is SIMPLY showing/hiding the correct form and form text.
+    //use validation, not authentication. the validation will prevent duplicates in the database. when you do a fetch .then() you should do a .catch() which catches errors. Don't do a .then() and just leave it b/c need a .catch() after that.
+    //this should be done after the event listener for the login button.
+    //maybe use cookies to keep someone logged in on the front end. It's easy to implement in JS.
+    id = event.target.dataset;
+    fetch("http://localhost:3000/api/v1/users/"`${id}`);
+    //this is going to be a post. It will take the event value when you hit submit and post it.
+    //on signup it will be a post after you create a new user on the JavaScript side. new user and feed in target value of name. It'll create a user on the JS side and on the backend side. After it posts to back end and creats a user in the JS User Class, you want to store that user as loggedInUser.
+    console.log("SIGNUP CLICKED");
+  } else if (event.target.id === "login") {
+    //this goes into a user creation/user login function: it's going to compare the data value to the user's array (up top).
+    //I'm simply making the login form look corretly here.
+    console.log("LOGIN CLICKED");
+  } else if (event.target.id === "logout") {
+    console.log("LOGOUT CLICKED");
+    //when logout happens we are going to remove the loggedInUser and hide both of the page elements.
+    document.getElementById("question-form").style.display = "none";
+    document.getElementById("category-id").style.display = "none";
+  }
+  if (user) {
+    //use an if statment to show the displays once the user is logged in.
+    document.getElementById("question-form").style.display = "block";
+    document.getElementById("category-id").style.display = "block";
+  }
+}
 
 function createFilteredQuestionsForCategory(event) {
   filteredQuestions = questions.filter(question => {
@@ -67,17 +105,15 @@ function submitListener() {
 }
 
 function postAnsweredQuestion(event) {
-  const body = {
-
-  }
-  debugger
-  const postQuestionURL = "http://localhost:3000/api/v1/answered_questions"
+  const body = {};
+  debugger;
+  const postQuestionURL = "http://localhost:3000/api/v1/answered_questions";
   fetch(`postQuestionURL/${event.target.dataset.id}`, {
-    method: 'PATCH',
+    method: "PATCH",
     headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
+      "Content-Type": "application/json",
+      Accept: "application/json"
     },
-    body: JSON.stringify(body),
-  })
+    body: JSON.stringify(body)
+  });
 }
--- app/js/user.js
+++ app/js/user.js
@@ -5,8 +5,6 @@ class User {
     User.all.push(this);
   }
 
-  showUser() {
-    return `<li>${this.name}</li>`;
-  }
+  //we're going to create user and handle the login and the logout in the class. The logout needs to just clear the global variable...the global variable is loggedInUser.
 }
 User.all = [];
